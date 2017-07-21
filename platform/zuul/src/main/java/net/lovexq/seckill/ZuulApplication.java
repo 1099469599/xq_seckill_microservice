545d558a61aa23e47a2fd199688101f4a6e6e792
LuPindong <lupindong@gmail.com>
2017-07-21 13:16:42 +0800

    修改一些配置

--- platform/zuul/src/main/java/net/lovexq/seckill/ZuulApplication.java
+++ platform/zuul/src/main/java/net/lovexq/seckill/ZuulApplication.java
@@ -1,6 +1,5 @@
 package net.lovexq.seckill;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -14,8 +13,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableEurekaClient
 public class ZuulApplication {
-
-    @Value("")
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
