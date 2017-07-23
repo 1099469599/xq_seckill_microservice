789bd0f8ec85d271813b5a7039195ffffc4ae740
LuPindong <lupindong@gmail.com>
2017-07-23 12:01:05 +0800

    更新数据中心ip

--- application/background-crawler/src/test/java/net/lovexq/seckill/background/crawler/TestMQ.java
+++ application/background-crawler/src/test/java/net/lovexq/seckill/background/crawler/TestMQ.java
@@ -1,8 +1,25 @@
 package net.lovexq.seckill.background.crawler;
 
+import net.lovexq.seckill.background.crawler.support.rabbimq.MqProducer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * @author LuPindong
  * @time 2017-07-11 21:08
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestMQ {
+
+    @Autowired
+    private MqProducer mqProducer;
+
+    @Test
+    public void sendMessage() {
+        mqProducer.sendQueueMessage(null,"LianJiaCrawler.Check.Queue");
+    }
 }
